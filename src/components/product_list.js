18658ecc37e563f355b5eb0a492902e2844c50cd
Veikko Soininen <veikko.soininen@gmail.com>
2021-07-12 14:40:33 +0300

    Render products to one table with collected button in the middle

--- src/components/product_list.js
+++ src/components/product_list.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Fragment } from 'react'
 
 export const createProducts = (list, productNames, checkChanged, removed) => {
   const notCollected = list.products.filter(product => !product.collected)
@@ -7,50 +6,51 @@ export const createProducts = (list, productNames, checkChanged, removed) => {
 
   const renderList = l => {
     return (
-      <div className="MarginHorizontal">
-        <table style={{ width: '100%' }}>
-          <tbody className="MarginHorizontal">
-            {l.map(product => {
-              return (
-                <tr className={product.collected ? 'Collected' : ''} key={product.id}>
-                  <td className="TextAlignLeft">{product.id ? productNames[product.id] : ''}</td>
-                  <td className="ProductLine TextAlignCenter">{product.amount} pcs</td>
-                  <td className="TextAlignCenter">
-                    <label>Collected:
-                      <input className="form-check-input"
-                        type="checkbox"
-                        style={{ marginLeft: '10px' }}
-                        checked={product.collected ? true : false}
-                        onChange={event => checkChanged(event, list, product)}>
-                      </input>
-                    </label>
-                  </td>
-                  <td className="TextAlignRight">
-                    <button className="btn-sm btn-danger" onClick={() => removed(list, [product])}>Remove</button>
-                  </td>
-                </tr >
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
+      l.map(product => {
+        return (
+          <tr className={product.collected ? 'Collected' : ''} key={product.id}>
+            <td className="TextAlignLeft">{product.id ? productNames[product.id] : ''}</td>
+            <td className="ProductLine TextAlignCenter">{product.amount} pcs</td>
+            <td className="TextAlignCenter">
+              <label>Collected:
+                <input className="form-check-input"
+                  type="checkbox"
+                  style={{ marginLeft: '10px' }}
+                  checked={product.collected ? true : false}
+                  onChange={event => checkChanged(event, list, product)}>
+                </input>
+              </label>
+            </td>
+            <td className="TextAlignRight">
+              <button className="btn-sm btn-danger" onClick={() => removed(list, [product])}>Remove</button>
+            </td>
+          </tr >
+        )
+      })
     )
   }
 
   const clearCollectedButton = () => (
-    <div className="ViewFull">
-      <button className="btn-sm btn-secondary MarginVertical"
-        onClick={() => removed(list, collected)}>Clear collected
-      </button>
-    </div>
+    <tr>
+      <td colSpan="4">
+        <div className="TextAlignCenter">
+          <button className="btn-sm btn-secondary MarginVertical"
+            onClick={() => removed(list, collected)}>Clear collected
+          </button>
+        </div>
+      </td>
+    </tr>
   )
 
   return (
-    <Fragment>
-      {renderList(notCollected)}
-      {collected.length ? clearCollectedButton() : null}
-      {renderList(collected)}
-    </Fragment>
-
+    <div className="MarginHorizontal">
+      <table style={{ width: '100%' }}>
+        <tbody className="MarginHorizontal">
+          {renderList(notCollected)}
+          {collected.length ? clearCollectedButton() : null}
+          {renderList(collected)}
+        </tbody>
+      </table>
+    </div>
   )
 }
