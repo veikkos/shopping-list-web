8e6cda1e17cd4689234c2ef300a26f760c37aefa
Veikko Soininen <veikko.soininen@gmail.com>
2021-07-14 18:44:09 +0300

    Add GitHub icon to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Footer = () => (
   <div className="Footer Flex">
-    <div className="FlexGrow TextAlignCenter">
+    <div className="FlexGrow TextAlignCenter container">
       <a target="_blank"
         rel="noreferrer"
-        href={process.env.REACT_APP_SOURCE_CODE_URL}>Source Code @ GitHub</a>
+        href={process.env.REACT_APP_SOURCE_CODE_URL}>Source Code @ GitHub
+        <i className="bi-github Title"
+          style={{ marginLeft: '5px' }}
+          role="img"
+          aria-label="GitHub" />
+      </a>
     </div>
   </div>
 )
