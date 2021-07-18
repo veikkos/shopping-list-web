ec4cd6a61aa7234cac1bd4afd59062089456f3fe
Veikko Soininen <veikko.soininen@gmail.com>
2021-07-18 22:55:49 +0300

    Add Logo to loading view

--- src/components/progress.js
+++ src/components/progress.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Loader from 'react-loader-spinner'
+import logo from '../media/logo192.png'
 
 const Progress = ({ show }) => (
-  <Loader
-    visible={show}
-    type="ThreeDots"
-    color="#CC3F0C"
-    height={100}
-    width={100}
-  />
+  <div className='ProgressContainer'>
+    <img
+      className='Logo'
+      src={logo}
+      alt="Logo"
+    />
+    <Loader
+      visible={show}
+      type="ThreeDots"
+      color="#CC3F0C"
+      height={100}
+      width={100}
+    />
+  </div>
 )
 
 export default Progress
