aaa8b4b3091d66d33e1f4add3bb2f0d4756ed0a0
Veikko Soininen <veikko.soininen@gmail.com>
2024-07-05 22:15:00 +0300

    Update share URL after changing to GitHub pages

--- src/util/share.js
+++ src/util/share.js
@@ -1,1 +1,4 @@
-export const shareUrl = id => `${window.location.origin}?share=${id}`
+const url = new URL(window.location.href)
+const urlWithoutParams = `${url.protocol}//${url.hostname}${url.pathname}`
+
+export const shareUrl = id => `${urlWithoutParams}?share=${id}`
